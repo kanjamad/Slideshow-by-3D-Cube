30156464e5dc88bbdf98dbf03d64cc8ca4d9f1e9
kanjamad bosel <kanjamadpor@gmail.com>
2019-06-19 12:08:13 -0700

    each cube will rotate by 90 deg -- Done for left arrow

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 console.log("Hi....!")
 
+let x = 0;
+
 document.querySelector('.left-arrow').addEventListener('click', () => {
+    x += 90;
     // console.log("click on left")
     const cubes = document.querySelectorAll('.cube');
-    console.log(cubes);
-    [...cubes].forEach(cube => cube.style.transform = 'rotateY(90deg');
+    // console.log(cubes);
+    [...cubes].forEach(cube => cube.style.transform = `rotateY(${x}deg`);
+    console.log(x);
 });
