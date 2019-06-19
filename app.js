41d5859a1969a615822c3935110050ebbe7f9fdd
kanjamad bosel <kanjamadpor@gmail.com>
2019-06-19 12:18:21 -0700

    create function expression

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ console.log("Hi....!")
 
 let x = 0;
 
-document.querySelector('.left-arrow').addEventListener('click', () => {
-    x += 90;
-    // console.log("click on left")
+const rotate = () => {
     const cubes = document.querySelectorAll('.cube');
-    // console.log(cubes);
     [...cubes].forEach(cube => cube.style.transform = `rotateY(${x}deg`);
-    console.log(x);
+    // console.log(x);
+}
+
+document.querySelector('.left-arrow').addEventListener('click', () => {
+    // console.log("click on left")
+    x += 90;
+    rotate();
 });
